86bffc53950187f60686f3c16f978c3da8d7e20b
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-09-04 11:59:34 -0700

    Create GetAddressMatchSimkey.rs

---
+++ GetAddressMatchSimkey.rs
@@ -0,0 +1,23 @@
+// Get your API key at www.interzoid.com/register
+
+// rename to main.rs in src directory for simple "Cargo run" within project directory
+
+// Cargo.html:
+//
+// [dependencies]
+// reqwest = { version = "0.11", features = ["json"] }
+// tokio = { version = "1", features = ["full"] }
+
+#[tokio::main]
+
+async fn main() -> Result<(), reqwest::Error> {
+    let res = reqwest::get("https://api.interzoid.com/getaddressmatchadvanced?license=YOURAPIKEY&address=500 main street suite 101&algorithm=narrow").await?;
+
+    println!("Status: {}", res.status());
+
+    let body = res.text().await?;
+
+    println!("Body: {}", body);
+
+    Ok(())
+}
