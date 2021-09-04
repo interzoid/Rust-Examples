16732aaa08cc556b27493030e766aad1f227e527
Interzoid <50465512+interzoid@users.noreply.github.com>
2021-09-04 11:03:53 -0700

    Create GetCompanyMatchSimKey.rs

---
+++ GetCompanyMatchSimKey.rs
@@ -0,0 +1,23 @@
+// Get your API key at www.interzoid.com/register
+
+// rename to main.rs in src directory for simple "Cargo run" within project directory
+
+// Cargo.html:
+//
+// [dependencies]
+// reqwest = { version = "0.11", features = ["json"] }
+// tokio = { version = "1", features = ["full"] }
+
+#[tokio::main]
+
+async fn main() -> Result<(), reqwest::Error> {
+    let res = reqwest::get("https://api.interzoid.com/getcompanymatchadvanced?license=YOURAPIKEY&company=ibm&algorithm=wide").await?;
+
+    println!("Status: {}", res.status());
+
+    let body = res.text().await?;
+
+    println!("Body: {}", body);
+
+    Ok(())
+}
